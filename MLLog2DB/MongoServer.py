4934aeedd7684a5d784dbd005f0c40f57aa1d7c2
Oldentomato <ender1412@naver.com>
2022-12-29 13:34:08 +0900

    db connect url 부분 수정

--- MLLog2DB/MongoServer.py
+++ MLLog2DB/MongoServer.py
@@ -4,16 +4,14 @@ from dataclasses import dataclass
 
 @dataclass
 class SetMongoDB:
-    db_username : str
-    db_password: str
-    db_cluster_name : str
+    db_url : str
     db_document_name : str
     db_collection_name : str
     port : int
 
     def Connect_DB(self) -> object:
         #self.__변수명 : 비공개 속성
-        self.__mongo_url = "mongodb+srv://"+self.db_username+":"+self.db_password+"@"+self.db_cluster_name+".g7o5t.mongodb.net/?retryWrites=true&w=majority"
+        self.__mongo_url = self.db_url
         try:
             client = MongoClient(host = self.__mongo_url, port = self.port)
         except:
