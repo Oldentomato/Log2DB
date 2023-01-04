1d54ae76b1baa439d7d7860b9e3467e138f2622c
Oldentomato <ender1412@naver.com>
2023-01-04 12:56:36 +0900

    setup 파일내용 수정

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 import setuptools
 
 setuptools.setup(
-    name="Log2DB",
-    version="0.0.1",
+    name="log2db",
+    version="0.0.1.1",
     license='MIT',
     author="WooSung Jo",
     author_email="jwsjws99@gmail.com",
     description="Send Deep Learning Training,Test Log To DB",
     long_description=open('README.md').read(),
     url="https://github.com/Oldentomato/Log2DB",
-    package_dir={"":"Log2DB"},
-    packages=setuptools.find_packages(where="Log2DB"),
+    packages=setuptools.find_packages(),
     classifiers=[
         # 패키지에 대한 태그
         "Programming Language :: Python :: 3",
